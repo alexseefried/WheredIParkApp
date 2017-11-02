7a7d4a865e40ef76f609f9ed6a0a69feedd5c94a
alexseefried <33331375+alexseefried@users.noreply.github.com>
2017-11-02 13:38:52 -0700

    Add files via upload

---
+++ app/src/main/java/com/example/t00226240/wheresmycar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.t00226240.wheresmycar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
